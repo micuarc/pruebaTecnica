b35986a6a66a262020f7a8d01652be0c195a94bc
micuarc <adacontrerasd@gmail.com>
2023-10-04 18:45:08 -0300

    Modelos de las tablas de la BD

---
+++ app/Models/Dispositivo.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Dispositivo extends Model
+{
+    use HasFactory;
+    protected $table = 'dispositivo';
+    protected $fillable = [
+        'idModelo',
+        'idBodega',
+        'nombreDispositivo'
+    ];
+}
